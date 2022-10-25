23b33e17e955cca0e6fd015f24d2f2315502d76c
SamNyarko <snyar0006@algonquinlive.com>
2022-10-25 00:51:53 -0400

    .

---
+++ script.js
@@ -0,0 +1,22 @@
+const fetchPokemon = () => {
+    for (let i = 1; i< 2; i++){
+    const url = 'https://pokeapi.co/api/v2/pokemon/';
+    fetch(url)
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+
+        const pokemon = {
+        name: data.name,
+        id:  data.id,
+        image: data.sprites['front_default'],
+        type: data.types.map((type) => type.type.name.join(', '))
+        };
+        console.log(pokemon)
+    });
+}
+};
+
+fetchPokemon();
