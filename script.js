c6d3f3c2a78ff1ededc4ca0b4e35dc91bdf42d21
SamNyarko <snyar0006@algonquinlive.com>
2022-11-02 23:49:09 -0400

    loaded pokemon onto site

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 20
+const pokemon_count = 25
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -15,38 +15,51 @@ const colors = {
     flying:'#F5F5F5',
     fighting:'#E6E0D4',
     normal:'#F5F5F5'
-}
+};
+
+const main_types = Object.keys(colors);
+
 
 
 const fetchPokemons = async () => {
     for(let i = 1; i<= pokemon_count; i++){
-        await getPokemon(i)
+        await getPokemon(i);
     }
 }
 
 const getPokemon = async (id) => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/'
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
-    const data = await res.json()
-    createPokemonCard(data)
+    const pokemon = await res.json()
+    createPokemonCard(pokemon)
 }
+
+fetchPokemons();
 
 const createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement('div')
     pokemonEl.classList.add('pokemon')
+    const poke_types = pokemon.types.map(el => el.type.name);
+    const type = main_types.find(type => poke_types.indexOf(type) > -1);
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const color = colors[type];
+
+    pokemonEl.style.backgroundColor = color
+
     const pokemonInnerHTML = 
-    ` <div class="img-container">
-    <img src="/images/bulbasur.png" alt="">
+    `
+    <div class="img-container">
+    <img src="https://pokeres.bastionbot.org/images/pokemon/{${pokemon.id1} }.png">
  </div>
  <div
  class="info">
-     <span class="number">#001</span>
-     <h3 class="type">Bulbasaur</h3>
-     <small>Type: <span>grass</span></small>
- </div>`
+     <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+     <h3 class="name">${name}</h3>
+     <small class="type">Type: <span>${type}</span></small>
+ </div> `
+ 
 
  pokemonEl.innerHTML = pokemonInnerHTML
 
  poke_container.appendChild(pokemonEl)
 }
-fetchPokemons()
