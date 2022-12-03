952ba822697e7980f9683428087581a11d2deaf1
sam nyarko <nyar0006@algonquinlive.com>
2022-12-03 14:04:57 -0500

    .

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const poke_container = document.getElementById('poke-container')
-let pokemon_count = 25
+let pokemon_count = 150
 const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-    electric:'#FCF7DE',
-    water:'#DEF3FD',
-    ground: '#f4e7da',
+    fire: '#EE8130',
+    grass: '#7AC74C',
+    electric:'#F7D02C',
+    water:'#6390F0',
+    ground: '#705746',
     rock:'#d5d5d4',
-    fairy:'#fceaff',
-    poison:'#98d7a5',
-    bug:'#f8d5a3',
+    fairy:'#D685AD',
+    poison:'#A33EA1',
+    bug:'#A6B91A',
     dragon:'#97b3e6',
-    psychic:'#eaeda1',
-    flying:'#F5F5F5',
-    fighting:'#E6E0D4',
-    normal:'#F5F5F5'
+    psychic:'#F95587',
+    flying:'#A98FF3',
+    fighting:'#C22E28',
+    normal:'#A8A77A'
 };
 
 const main_types = Object.keys(colors);
+
+const $main = document.getElementById('main')
 
 
 
 
 
 const fetchPokemons = async () => {
-    for(let i = 1; i<= pokemon_count; i++){
+    for(let i = 1; i<=20; i++){
         await getPokemon(i);
     }
 }
@@ -36,7 +38,173 @@ const getPokemon = async (id) => {
     createPokemonCard(pokemon)
 }
 
+
+
 fetchPokemons();
+
+const pokeCard = document.getElementById('card')
+
+
+const selectPokemon = async (id) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    displayPopup(pokemon);
+}
+
+
+
+const displayPopup = (pokemon) => {
+    const poke_types = pokemon.types.map(el => el.type.name);
+
+    const type = main_types.find(type => poke_types.indexOf(type) > -1);
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+
+    const  htmlString = `
+    <div id="pokeBg" class="popup">
+    <div class="container">
+    <button id="close">
+    <i class="fa-solid fa-xmark fa-6x"></i>
+    </button>
+    <nav class="logo2">
+    
+        <div class="nav-container">
+
+            <button onclick="myFunction()" id="poke-menu" class="poke-menu dropbtn">
+                <i class="fa-brands fa-microsoft fa-4x"></i>
+            </button>
+            <a class="logo" href="index.html">
+                <img   href="index.html" src="/images/pokedex.png" alt="">
+            </a>
+        </div>
+    </nav>
+    <div id="square" class="square">
+        <div id="poke-container" class="poke-container">
+
+        </div>
+       
+    </div>
+            <!-- Front and back arrows -->
+        <span class="leftarrow">
+            <a href="ivysaur.html">
+
+                <button  class="btnarrows desktop">
+                    <i id="leftarrow"class="fa-solid fa-chevron-left fa-8x "></i>
+                </button>
+                <button class="btnarrows tablet">
+                    <i id="leftarrow"class="fa-solid fa-chevron-left fa-6x "></i>
+                </button>
+                <button class="btnarrows mobile">
+                    <i id="leftarrow"class="fa-solid fa-chevron-left fa-4x "></i>
+                </button>
+            </a>
+            </span>
+            
+            <span class="rightarrow">
+                <a href="">
+
+                    <button class="btnarrows desktop">
+                        <i id="rightarrow"class="fa-solid fa-chevron-right fa-8x "></i>
+                    </button>
+                    <button class="btnarrows tablet">
+                        <i id="rightarrow"class="fa-solid fa-chevron-right fa-6x "></i>
+                    </button>
+                    <button class="btnarrows mobile">
+                        <i id="rightarrow"class="fa-solid fa-chevron-right fa-4x"></i>
+                    </button>
+                </a>
+                </span>
+
+        <!-- Pokemon information -->
+<div class="pokemon-full">
+        <img  id="pokemon-img" class="pokemon-img bounce-1" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"> >
+        <div class="poke-info">
+            <div class="leftside">
+                
+                <p class="num-id">#${pokemon.id.toString().padStart(3, '0')}</p>
+                <h3 class="poke-name">${name}</h3>
+                <p class="full-type">${type}</p>
+                
+                <div class="attributes">
+                    <table>
+                        <tr>
+                            <td>Height:</td>
+                            <td>${pokemon.height}</td>
+                        </tr>
+                        <tr>
+                            <td>Weight:</td>
+                            <td>${pokemon.weight} Ibs</td>
+                        </tr>
+                        <tr>
+                            <td>Abilities:</td>
+                            <td>${pokemon.abilities}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>    
+        </div>
+        <div class="types">
+            <h5>Type</h5>
+            <p>${type}</p>
+        </div>
+
+        <div class="rightside">
+                
+            <h4>
+                Stats
+            </h4>
+            <div class="attribute-chart">
+            <h6 class="stat-title">
+                HP
+            </h6>
+            <p class="stats-number">
+                45
+            </p>
+            <h6 class="stat-title">
+                Attack
+            </h6>
+            <p class="stats-number">
+                49
+            </p>
+            <h6 class="stat-title">
+                Defence
+            </h6>
+            <p class="stats-number">
+                49
+            </p>
+            <h6 class="stat-title">
+                Speed
+            </h6>
+            <p class="stats-number">
+                45
+            </p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+    `
+    $main.innerHTML = htmlString + $main.innerHTML ;
+ 
+    
+    const $close = document.getElementById('close');
+
+$close.addEventListener('click', function (){
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+});
+
+const popbg = document.getElementById('pokeBg')
+
+
+
+const color = colors[type];
+
+popbg.style.backgroundColor = linearGradient(color,black)
+
+};
 
 
 
@@ -47,17 +215,21 @@ const createPokemonCard = (pokemon) => {
 
     const type = main_types.find(type => poke_types.indexOf(type) > -1);
 
+   
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
     const color = colors[type];
 
     pokemonEl.style.backgroundColor = color
 
-    const pokemonInnerHTML = 
     
+
+    
+
+    const pokemonInnerHTML = 
     `
-    <div>
-    <button id="card-btn${pokemon.id}">
+    <div id="card">
+    <button id="card-btn${pokemon.id}" onclick="selectPokemon(${pokemon.id})">
     <div class="img-container">
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
  </div>
@@ -69,8 +241,7 @@ const createPokemonCard = (pokemon) => {
      </div> 
      </button>
      <div class="status">
-     <input type="checkbox" id="caught" name="catch" value="Catch">
-    <span>Caught</span>
+     <button id="caught">Caught</button>
      </div>
      `
  
@@ -79,16 +250,10 @@ const createPokemonCard = (pokemon) => {
  pokemonEl.innerHTML = pokemonInnerHTML
 
  
+ 
+
 
  poke_container.appendChild(pokemonEl);
-
- const $btn = document.getElementById('card-btn1')
-
-
-
-$btn.addEventListener('click', function() {
-    location.href = "bulbaseaur.html"; 
-   });
 
   
 
@@ -100,19 +265,19 @@ $btn.addEventListener('click', function() {
 const $load_more = document.getElementById('load-more')
 $load_more.addEventListener('click', function(){
     const fetchPokemons = async () => {
-        for(let i = 1; i <= 25; i++){
+        for(let i = 21; i<=40; i++){
             await getPokemon(i);
         }
     }
     
     const getPokemon = async (id) => {
-        
-        
         const url = `https://pokeapi.co/api/v2/pokemon/${id}`
         const res = await fetch(url)
         const pokemon = await res.json()
         createPokemonCard(pokemon)
     }
+    
+    
     
     fetchPokemons();
     
@@ -126,3 +291,18 @@ function myFunction() {
   };
 
 
+
+
+
+  function catchPokemon(){
+    const $button = document.getElementsByClassName('status');
+
+    $button.addEventListener('click', function(){
+      for(let i=0; i < button.length; i++){
+          console.log('hello');
+      }
+      catchPokemon();
+    });
+
+    
+}
